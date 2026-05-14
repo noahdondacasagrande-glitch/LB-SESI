1e312b91098916f85fe5f3b21debbf2a53005d9f
noahdondacasagrande-glitch <noahdondacasagrande@gmail.com>
2026-05-14 07:55:23 -0300

    Add files via upload

--- scr/script.js
+++ scr/script.js
@@ -1,11 +1,14 @@
 window.addEventListener('scroll', ()=> {
     const nav = document.querySelector('nav')
+    const footer = document.querySelector('footer')
 
 
     if(window.scrollY > 0){
         nav.classList.add('cs')
+        nav.classList.add('ft')
     }else{
         nav.classList.remove('cs')
+        footer.classList.remove('ft')
     }
 })
 
