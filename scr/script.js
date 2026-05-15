21088e69e72bf5316edcd95589fc6482e882e0ec
noahdondacasagrande-glitch <noahdondacasagrande@gmail.com>
2026-05-15 11:08:30 -0300

    Add files via upload

--- scr/script.js
+++ scr/script.js
@@ -19,3 +19,25 @@ function navTo(id){
         block:'start'
     })
 }
+
+
+const cards = document.querySelectorAll('#s2, .card');
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            cards.forEach((card, index) => {
+                setTimeout(() => {
+                    card.classList.add('visible');
+
+                }, index * 200)
+            });
+        }else{
+            cards.forEach((card) => {
+                    card.classList.remove('visible');
+            });
+        }
+    })
+}, {threshold: 0.2})
+
+observer.observe(document.querySelector('#s2'))
